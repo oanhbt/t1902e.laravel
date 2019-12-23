1c8f5ebc04c9b6e7a7d4c25d1ff2f805751066a7
LaiDuke <duclvth1902009@fpt.edu.com>
2019-12-23 21:33:32 +0700

    laiduc

---
+++ LaiDuke/blog/database/migrations/2019_12_23_210957_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('cover');
+            $table->text('content');
+            $table->bigInteger('user_id');
+            $table->bigInteger('views');
+            $table->bigInteger('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
