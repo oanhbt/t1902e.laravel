89e5dae037f095ee9a47d18cfb44322185d2c5be
vuvansan <sanvvth1812002@fpt.edu.vn>
2019-12-30 18:35:58 +0700

    a

--- san/blog/resources/views/layouts/app.blade.php
+++ san/blog/resources/views/layouts/app.blade.php
@@ -33,6 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                      <li class="nav-item">
+                        <a href="#">hihihaha</a>
+                      </li>
 
                     </ul>
 
