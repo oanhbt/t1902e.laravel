f84b6b4c69d2d2e7d28fcbd1e919ee189535c793
vuvansan <sanvvth1812002@fpt.edu.vn>
2019-12-30 18:36:18 +0700

    a

--- san/blog/app/Post.php
+++ san/blog/app/Post.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDelete;
 class Post extends Model
 {
     //
+    use SoftDelete;
+    protected $date=['delete_at'];
 }
